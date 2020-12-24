7579c19f4f4ff7c0164c0f92e762787f4ec3b0ae
fu <semarusika@gmail.com>
2020-12-25 07:53:37 +0900

    use-state

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
+import Counter from './Counter'
 
 interface AppProps {
   message?: string
 }
 
 const App: React.FunctionComponent<AppProps> = ({message}) => {
-  return <div>{message}</div>;
+  return <div><Counter/></div>;
 };
 
 App.defaultProps = {
   message: ' Hello, default props',
 }
-
 
 export default App;
