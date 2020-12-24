76d11da1f5b55145c2d2ec65957f9fe7879767e4
fu <semarusika@gmail.com>
2020-12-25 07:24:55 +0900

    function-component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
 interface AppProps {
-  message: string
+  message?: string
 }
 
-const App = (props: AppProps) => {
-  return <div>{props.message}</div>;
+const App: React.FunctionComponent<AppProps> = ({message}) => {
+  return <div>{message}</div>;
 };
+
+App.defaultProps = {
+  message: ' Hello, default props',
+}
+
 
 export default App;
